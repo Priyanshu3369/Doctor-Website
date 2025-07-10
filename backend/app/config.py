ae664d361bca5eb093fdf72bdcfc878f8bc38999
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-07-10 12:54:25 +0530

    fresh start

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
-    MONGO_URI = os.getenv("MONGO_URI")
